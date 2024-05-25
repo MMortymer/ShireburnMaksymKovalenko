3dc27b63ccc8c397f918bddb907f88134cc22435
Maksym Kovalenko <maxeladze@gmail.com>
2024-05-25 15:44:55 +0200

    initial commit vue3 + tailwindcss

---
+++ src/components/EmployeesList.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref } from 'vue'
+</script>
+
+<template>
+  <div class="flex justify-center">
+    <h1 class="text-3xl">Employees List</h1>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import EmployeesList from '../components/EmployeesList.vue'
+</script>
+
+<template>
+  <main class="p-8">
+    <EmployeesList />
+  </main>
+</template>
