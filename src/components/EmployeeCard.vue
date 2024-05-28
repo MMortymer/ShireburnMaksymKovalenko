8487e0e4bae73e7458191dcb46ee56c34c95f8ea
Maksym Kovalenko <maxeladze@gmail.com>
2024-05-28 13:02:55 +0200

    fix responsive styling

--- src/components/EmployeeCard.vue
+++ src/components/EmployeeCard.vue
@@ -11,10 +11,10 @@ defineProps({
     :to="{ name: 'employeeDetails', params: { id: employee.Id } }"
     class="border rounded-xl px-5 py-2 hover:scale-105 hover:border-cyan-500 transition-all"
   >
-    <div class="flex items-center gap-4">
+    <div class="flex items-center gap-2">
       <h3 class="flex text-xl text-black">{{ employee.FirstName }} {{ employee.LastName }}</h3>
       <p class="text-sm ml-auto">
-        {{ birthdayToAgeString(employee.DateOfBirth) }}, {{ employee.Gender }}
+        {{ birthdayToAgeString(employee.DateOfBirth) }}, {{ employee.Gender.substring(0, 1) }}
       </p>
     </div>
 
--- src/components/EmployeesList.vue
+++ src/components/EmployeesList.vue
@@ -32,7 +32,7 @@ const filteredEmployees = computed(() => {
 
     <div
       v-else
-      class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 w-full sm:px-6 xl:px-24"
+      class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 w-full sm:px-6 xl:px-8"
     >
       <EmployeeCard
         v-for="employee in filteredEmployees"
